e4bdb830cd0b631f1bceee52260f3ee514a77580
klausefire <49187839+klausefire@users.noreply.github.com>
2023-03-15 18:17:58 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,54 @@
+// select the HTML elements
+const newTodoInput = document.querySelector('#new-todo-input');
+const addTodoButton = document.querySelector('#add-todo-button');
+const todoList = document.querySelector('#todo-list');
+
+// add event listener to the "Add" button
+addTodoButton.addEventListener('click', addTodo);
+
+// function to add a new to-do item
+function addTodo() {
+	// get the new to-do text from the input field
+	const newTodoText = newTodoInput.value.trim();
+
+	// check if the new to-do text is not empty
+	if (newTodoText !== '') {
+		// create a new li element for the to-do item
+		const todoItem = document.createElement('li');
+
+		// create a checkbox input element
+		const checkbox = document.createElement('input');
+		checkbox.type = 'checkbox';
+
+		// create a span element for the to-do text
+		const todoText = document.createElement('span');
+		todoText.textContent = newTodoText;
+
+		// create a button element to delete the to-do item
+		const deleteButton = document.createElement('button');
+		deleteButton.textContent = 'Delete';
+
+		// add the elements to the to-do item li element
+		todoItem.appendChild(checkbox);
+		todoItem.appendChild(todoText);
+		todoItem.appendChild(deleteButton);
+
+		// add event listener to the delete button
+		deleteButton.addEventListener('click', deleteTodo);
+
+		// add the to-do item to the todo-list ul element
+		todoList.appendChild(todoItem);
+
+		// clear the new to-do input
+		newTodoInput.value = '';
+	}
+}
+
+// function to delete a to-do item
+function deleteTodo() {
+	// get the parent li element of the delete button
+	const todoItem = this.parentNode;
+
+	// remove the li element from the todo-list ul element
+	todoList.removeChild(todoItem);
+}
